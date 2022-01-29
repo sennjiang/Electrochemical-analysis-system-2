6fd4fd5390d9ca8414b3c4a4387cae611ae34c94
sensen <64098056@qq.com>
2022-01-29 18:37:24 +0800

    更新 增加 id 查权限方法

--- EA-service/src/main/java/bluedot/electrochemistry/service/dao/BaseMapper.java
+++ EA-service/src/main/java/bluedot/electrochemistry/service/dao/BaseMapper.java
@@ -1,5 +1,6 @@
 package bluedot.electrochemistry.service.dao;
 
+import bluedot.electrochemistry.service.pojo.domain.Right;
 import bluedot.electrochemistry.service.pojo.domain.User;
 
 import java.util.List;
@@ -15,6 +16,8 @@ public interface BaseMapper {
     User loginByEmail(String account);
 
     Integer changePassword(String id,String password);
+
+    List<Right> getRights(String id);
 
 
 }
