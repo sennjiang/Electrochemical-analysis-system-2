126bc6a6069af21488c92e540f05765434d930f6
zero <3303361569@qq.com>
2022-02-16 22:50:59 +0800

    1. 补充缺失po类、以及部分注释

--- ea-commons/src/main/java/bluedot/electrochemistry/commons/entity/AlgorithmsLibrary.java
+++ ea-commons/src/main/java/bluedot/electrochemistry/commons/entity/AlgorithmsLibrary.java
@@ -3,17 +3,53 @@ package bluedot.electrochemistry.commons.entity;
 import java.time.LocalDateTime;
 
 /**
+ * 算法库表PO类
  * @Author zero
  * @Create 2022/2/15 19:45
  */
 public class AlgorithmsLibrary {
+    /**
+     * 算法库编号
+     */
     private Integer id;
+
+    /**
+     * 作者昵称（默认用户昵称 可改）
+     */
     private String author;
+
+    /**
+     * 算法昵称(默认为算法昵称 可改)
+     */
     private String name;
+
+    /**
+     * 算法地址 例：/1001/Algo01.java
+     */
     private String path;
+
+    /**
+     * 算法类型，映射如下：
+     * 1->降噪
+     * 2->平滑
+     * 3->基准线
+     * 4->其他
+     */
     private String type;
+
+    /**
+     * 保存至用户算法的次数
+     */
     private Integer saveTimes;
+
+    /**
+     * 使用此算法的次数
+     */
     private Integer useTimes;
+
+    /**
+     * 创建时间
+     */
     private LocalDateTime gmtCreate;
 
 
---
+++ ea-commons/src/main/java/bluedot/electrochemistry/commons/entity/UserFileConfig.java
@@ -0,0 +1,129 @@
+package bluedot.electrochemistry.commons.entity;
+
+import java.time.LocalDateTime;
+
+/**
+ * 用户文件配置PO类
+ * @Author zero
+ * @Create 2022/2/16 22:40
+ */
+public class UserFileConfig {
+    /**
+     * 编号
+     */
+    private Integer id;
+
+    /**
+     * 个人文件数量
+     */
+    private Integer fileCount;
+
+    /**
+     * 回收站文件数量
+     */
+    private Integer binCount;
+
+    /**
+     * 用户编号
+     */
+    private Integer userId;
+
+    /**
+     * 删除类型，映射关系如下：
+     * 0->删除最早的文件(默认)
+     * 1->删除最大的文件
+     */
+    private Integer deleteType;
+
+    /**
+     * 上传个人算法数量
+     */
+    private Integer alogCount;
+
+    /**
+     * 修改时间
+     */
+    private LocalDateTime gmtModify;
+
+    public UserFileConfig() {
+    }
+
+    public UserFileConfig(Integer id, Integer fileCount, Integer binCount, Integer userId, Integer deleteType, Integer alogCount, LocalDateTime gmtModify) {
+        this.id = id;
+        this.fileCount = fileCount;
+        this.binCount = binCount;
+        this.userId = userId;
+        this.deleteType = deleteType;
+        this.alogCount = alogCount;
+        this.gmtModify = gmtModify;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getFileCount() {
+        return fileCount;
+    }
+
+    public void setFileCount(Integer fileCount) {
+        this.fileCount = fileCount;
+    }
+
+    public Integer getBinCount() {
+        return binCount;
+    }
+
+    public void setBinCount(Integer binCount) {
+        this.binCount = binCount;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getDeleteType() {
+        return deleteType;
+    }
+
+    public void setDeleteType(Integer deleteType) {
+        this.deleteType = deleteType;
+    }
+
+    public Integer getAlogCount() {
+        return alogCount;
+    }
+
+    public void setAlogCount(Integer alogCount) {
+        this.alogCount = alogCount;
+    }
+
+    public LocalDateTime getGmtModify() {
+        return gmtModify;
+    }
+
+    public void setGmtModify(LocalDateTime gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+
+    @Override
+    public String toString() {
+        return "UserFileConfig{" +
+                "id=" + id +
+                ", fileCount=" + fileCount +
+                ", binCount=" + binCount +
+                ", userId=" + userId +
+                ", deleteType=" + deleteType +
+                ", alogCount=" + alogCount +
+                ", gmtModify=" + gmtModify +
+                '}';
+    }
+}
